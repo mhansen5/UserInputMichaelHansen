c4af19e6dfb54e3cff5ce4431bcd3e1782f72bea
Michael Hansen <gearsofwarfan97@gmail.com>
2020-01-31 20:28:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserInputMichaelHansen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Michael Hansen");
+            Console.WriteLine("Type what you want say, then press enter");
+
+            string testString;
+
+            testString = Console.ReadLine();
+            Console.WriteLine("You typed:  ");
+            Console.WriteLine(testString);
+
+        }
+    }
+}
